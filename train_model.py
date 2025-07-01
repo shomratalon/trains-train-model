977c37354473019043077b0613f5f320ac71a9a2
shomratalon <14982269+shomratalon@users.noreply.github.com>
2025-07-01 16:11:09 +0300

    Typo

--- train_model.py
+++ train_model.py
@@ -61,7 +61,7 @@ if __name__ == "__main__":
     queue_name = os.getenv('INPUT_QUEUE_NAME')
     os.environ["CLEARML_API_ACCESS_KEY"] = os.getenv('INPUT_CLEARML_API_ACCESS_KEY')
     os.environ["CLEARML_API_SECRET_KEY"] = os.getenv('INPUT_CLEARML_API_SECRET_KEY')
-    os.environ["TCLEARML_API_HOST"] = os.getenv('INPUT_CLEARML_API_HOST')
+    os.environ["CLEARML_API_HOST"] = os.getenv('INPUT_CLEARML_API_HOST')
     cloned_task = clone_and_queue(template_task=base_task_id, queue=queue_name)
     print(f'::set-output name=CLONED_TASK::{cloned_task.id}')
     print(f'::set-output name=TASK_STATUS::{cloned_task.get_status()}')
